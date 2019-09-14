7a58232bedead98a72281453dd7356d4326a4824
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Pro.local>
2019-09-14 12:15:25 -0700

    first

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<script>
+	//global libraries to init on mount
+	import AOS from 'aos';
+
+	//import specific lifecycle methods
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		AOS.init();
+	});
+	
+	//import components
+	import Card from "./components/Card.svelte";
+
+	//some local state
+	let events = [
+		{
+			name: "Super Party",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa esse id, ut dolorum maxime iure dolore ipsum sequi voluptates, fugiat nemo! Earum quo enim cupiditate cum, ut est quibusdam quis!",
+			image: "https://source.unsplash.com/collection/494263/800x601"
+		},
+		{
+			name: "Beach Day",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa esse id, ut dolorum maxime iure dolore ipsum sequi voluptates, fugiat nemo! Earum quo enim cupiditate cum, ut est quibusdam quis!",
+			image: "https://source.unsplash.com/collection/494263/800x600"
+		},
+		{
+			name: "Another Party",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa esse id, ut dolorum maxime iure dolore ipsum sequi voluptates, fugiat nemo! Earum quo enim cupiditate cum, ut est quibusdam quis!",
+			image: "https://source.unsplash.com/collection/494263/800x602"
+		}
+	]
+
+	//another way to organize your data is keep it in other files
+	import locations from "./data/locations";
+
+</script>
+
+<style type="text/scss">
+	.main-wrapper {
+		max-width: 1000px;
+		margin: auto;
+	}
+</style>
+
+<div class="main-wrapper py-5">
+		<div class="row">
+			<div class="col px-3">
+				<h1>Events</h1>
+			</div>
+		</div>
+		<div class="row">
+			{#each events as event}
+				<div class="col-4 p-3">
+					<Card name={event.name} description={event.description} image={event.image}/>
+				</div>
+			{/each}
+		</div>
+		<div class="row">
+			<div class="col px-3">
+				<h1>Locations</h1>
+			</div>
+			<div class="col-12 px-3">
+				{#each locations as location}
+					<h4>{location}</h4>
+				{/each}
+			</div>
+		</div>
+</div>
---
+++ src/components/Card.svelte
@@ -0,0 +1,36 @@
+<script>
+  //listen for props
+  export let image;
+  export let name;
+  export let description;
+</script>
+
+<style type="text/scss">
+  .card {
+    height: 500px;
+    box-shadow: 0 0 2px 4px rgba(0, 0, 0, 0.322);
+
+    &__image {
+      background-position: center;
+      background-size: cover;
+    }
+
+    &__details {
+      box-sizing: border-box;
+    }
+  }
+</style>
+
+<div class="card w-100">
+  <div class="flex flex-column h-100">
+    <div class="card__image w-100 h-50" style="background-image: url({image})">
+      <!-- Image  -->
+    </div>
+    <div class="card__details w-100 h-50 px-3 py-2">
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </div>
+  </div>
+</div>
+
+
